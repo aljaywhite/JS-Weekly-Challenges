5e621e9b3cfc195964102112cfeb9981eb412e8d
Alan White <aljaywhite23@gmail.com>
2016-05-26 14:18:29 -0400

    Added button and css styling to button--because I learned it and it seemed like a fun idea

--- Week3/app.js
+++ Week3/app.js
@@ -1,2 +1,8 @@
 //$(".cost span").append("$6500");
 $("h2 span").text(' $6500');
+$("h2 span").hide();
+$("h2").append("<button>Reveal Answer!</button>");
+$("button").click(function() {
+	$("h2 span").show();
+	$(this).remove();
+});
